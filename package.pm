3933178dde0b8e55ecea1667557aa762db27cc75
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-11-21 17:07:34 +0100

    Add setValid method (for hub archive invalid pkg)

--- package.pm
+++ package.pm
@@ -19,9 +19,16 @@ sub new {
         version         => $o->get("version") || "",
         build           => $o->get("build") || "",
         date            => $o->get("date") || "",
-        install_date    => $o->get("install_date")
+        install_date    => $o->get("install_date"),
+        valid           => 1
     };
     return bless($this,ref($class) || $class);
 }
+
+sub setValid {
+    my ($self,$vInt) = @_;
+    $self->{valid} = $vInt;
+}
+
 
 1;
