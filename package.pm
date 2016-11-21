dab8193a5c3ce23772925b8bfc22c35ae014ae83
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-11-21 16:43:46 +0100

    Create package.pm

---
+++ package.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+# Namespace
+package perluim::package;
+
+# Nimsoft librairies !
+use lib "D:/apps/Nimsoft/perllib";
+use lib "D:/apps/Nimsoft/Perl64/lib/Win32API";
+use Nimbus::API;
+use Nimbus::PDS;
+use Nimbus::CFG;
+
+sub new {
+    my ($class,$o) = @_;
+    my $this = {
+        name            => $o->get("name"),
+        description     => $o->get("description") || "",
+        version         => $o->get("version") || "",
+        build           => $o->get("build") || "",
+        date            => $o->get("date") || "",
+        install_date    => $o->get("install_date")
+    };
+    return bless($this,ref($class) || $class);
+}
+
+1;
