7922de8cf3278f4364b02f4216afc379716d7524
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-11-24 15:05:52 +0100

    Split with archive.pm

--- hub.pm
+++ hub.pm
@@ -14,6 +14,7 @@ use Nimbus::CFG;
 # perluim packages
 use perluim::robot;
 use perluim::package;
+use perluim::archive;
 use perluim::probe;
 
 sub new {
@@ -119,36 +120,6 @@ sub getHashRobots {
     }
 }
 
-#
-# => Get Archive information
-#
-sub getArchivePackages {
-    my ($self,$name,$version) = @_;
-    my $PDS = pdsCreate();
-    my $clean_addr = substr($self->{addr},0,-4);
-    my ($RC,$NMS_RES) = nimNamedRequest("$clean_addr/automated_deployment_engine","archive_list",$PDS,10);
-    pdsDelete($PDS);
-
-    my %PackagesList = ();
-    if($RC == NIME_OK) {
-        my $PKG_PDS = Nimbus::PDS->new($NMS_RES);
-        for( my $count = 0; my $PKG_INFO = $PKG_PDS->getTable("entry",PDS_PDS,$count); $count++) {
-            my $PKG = new perluim::package($PKG_INFO);
-            if(defined($PKG->{version}) && $PKG->{version} ne "") {
-                $PackagesList{"$PKG->{name}_$PKG->{version}_$PKG->{build}"} = $PKG;
-            }
-            else {
-                $PKG->setValid(0);
-                $PackagesList{"$PKG->{name}_NV"} = $PKG;
-            }
-        }
-        return $RC,%PackagesList;
-    }
-    else {
-        return $RC,%PackagesList;
-    }
-}
-
 sub getEnv {
     my ($self,$var) = @_;
 
@@ -182,30 +153,10 @@ sub getInfo {
     return $RC,undef;
 }
 
-sub ade_addPackageSyncRule {
-    my ($self,$pkg) = @_;
-
-    my $PDS = pdsCreate();
-    pdsPut_PCH ($PDS,"name",$pkg->{name});
-    pdsPut_PCH ($PDS,"rule_type","ALL");
-    my $clean_addr = substr($self->{addr},0,-4);
-    my ($RC,$NMS_RES) = nimNamedRequest("$clean_addr/automated_deployment_engine","add_package_sync_rule",$PDS,10);
-    pdsDelete($PDS);
-    return $RC;
+sub archive {
+    return new perluim::archive($self);
 }
 
-sub deletePackage {
-    my ($self,$name,$version) = @_;
-
-    my $PDS = pdsCreate();
-    my $clean_addr = substr($self->{addr},0,-4);
-    pdsPut_PCH($PDS,"name","$name");
-    pdsPut_PCH($PDS,"version","$version");
-    my ($RC,$NMS_RES) = nimNamedRequest("$clean_addr/automated_deployment_engine","archive_delete",$PDS,10);
-    pdsDelete($PDS);
-
-    return $RC;
-}
 
 sub probeVerify {
     my ($self,$probeName) = @_;
