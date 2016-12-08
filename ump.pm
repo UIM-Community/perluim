f7b09c71583006a41f409af1231994a06aa376f9
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-12-08 12:07:05 +0100

    Update bad method API

--- ump.pm
+++ ump.pm
@@ -6,20 +6,17 @@ use LWP::UserAgent;
 package perluim::ump;
 
 sub new {
-    my ($class,@ump_servers,$login,$password) = @_;
+    my ($class,@ump_servers) = @_;
     my $this = {
         active => undef,
-        login => $login,
-        password => $password,
         pool => \@ump_servers
     };
     my $blessed = bless($this,ref($class) || $class);
-    $blessed->checkPool();
     return $blessed;
 }
 
 sub checkPool {
-    my ($self) = @_;
+    my ($self,$login,$password) = @_;
     my $RC = 1;
     if(scalar $self->{pool} == 1) {
         $self->{active} = @{$self->{pool}}[0];
@@ -30,7 +27,7 @@ sub checkPool {
     else {
         foreach my $umpAddr (@{$self->{pool}}) {
             my $request = HTTP::Request->new(GET => "$umpAddr/rest/version-info");
-            $request->authorization_basic( "$self->{login}", "$self->{password}" );
+            $request->authorization_basic( $login, $password );
             my $ua = LWP::UserAgent->new( ssl_opts => {
                 verify_hostname => 0,
                 SSL_verify_mode => 0x00
@@ -49,12 +46,7 @@ sub checkPool {
 
 sub isConnected {
     my ($self) = @_;
-    if(defined($self->{active})) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    return defined($self->{active}) ? 1 : 0;
 }
 
 1;
