0750559eb0fe0facbeccb02e4b090811f733eae4
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-12-16 11:12:57 +0100

    Add all local method + method refactoring

--- hub.pm
+++ hub.pm
@@ -26,23 +26,22 @@ sub new {
     }
     my $this = {
         name        => $o->get("name") || $o->get("hubname"),
-        robotname   => $o->get("robotname") || $addrArray[3] || "undefined",
+        robotname   => $o->get("robotname") || $addrArray[3],
         addr        => $o->get("addr") || $o->get("hubaddr"),
         domain      => $o->get("domain"),
         ip          => $o->get("ip") || $o->get("hubip"),
         port        => $o->get("port"),
         status      => $o->get("status"),
         version     => $o->get("version"),
-        origin      => $o->get("origin") || "BP2I",
+        origin      => $o->get("origin"),
         source      => $o->get("source"),
         last        => $o->get("last"),
-        origin      => $o->get("origin"),
         license     => $o->get("license"),
         sec_on      => $o->get("sec_on"),
         sec_ver     => $o->get("sec_ver"),
         ssl_mode    => $o->get("ssl_mode"),
         ldap        => $o->get("ldap"),
-        ldap_version => $o->get("ldap_version") || "unknown",
+        ldap_version => $o->get("ldap_version"),
         tunnel      => $o->get("tunnel") || "no",
         uptime      => $o->get("uptime") || 0,
         started     => $o->get("started") || 0
@@ -59,6 +58,16 @@ sub removeRobot {
 	pdsPut_PCH ($PDS,"name","controller");
 
 	my ($RC, $O) = nimNamedRequest("$self->{addr}", "removerobot", $PDS);
+	pdsDelete($PDS);
+    return $RC;
+}
+
+sub local_removeRobot {
+	my ($self,$robotName) = @_;
+	my $PDS = pdsCreate();
+	pdsPut_PCH ($PDS,"name",$robotName);
+	pdsPut_PCH ($PDS,"name","controller");
+	my ($RC, $O) = nimRequest("$self->{robotname}",48002, "removerobot", $PDS);
 	pdsDelete($PDS);
     return $RC;
 }
@@ -80,13 +89,13 @@ sub getRobots {
     }
 }
 
-sub getLocalRobots {
-    my ($self,$robotname) = @_;
+#
+# => Get an Array of robots in the instanciate HUB!
+#
+sub robotsArray {
+    my ($self) = @_;
     my $PDS = pdsCreate();
-    if(defined($robotname)) {
-        pdsPut_PCH($PDS,"name","$robotname");
-    }
-    my ($RC,$NMS_RES) = nimRequest("$self->{name}",48002,"getrobots",$PDS,10);
+    my ($RC,$NMS_RES) = nimNamedRequest("$self->{addr}","getrobots",$PDS,10);
     pdsDelete($PDS);
 
     my @RobotsList = ();
@@ -100,13 +109,10 @@ sub getLocalRobots {
     return $RC,@RobotsList;
 }
 
-#
-# => Get an Array of robots in the instanciate HUB!
-#
-sub getArrayRobots {
+sub local_robotsArray {
     my ($self) = @_;
     my $PDS = pdsCreate();
-    my ($RC,$NMS_RES) = nimNamedRequest("$self->{addr}","getrobots",$PDS,10);
+    my ($RC,$NMS_RES) = nimRequest("$self->{robotname}",48002,"getrobots",$PDS);
     pdsDelete($PDS);
 
     my @RobotsList = ();
@@ -123,10 +129,30 @@ sub getArrayRobots {
 #
 # => Get an Hash of robots in the instanciate HUB!
 #
-sub getHashRobots {
+sub robotsHash {
     my ($self) = @_;
     my $PDS = pdsCreate();
     my ($RC,$NMS_RES) = nimNamedRequest("$self->{addr}","getrobots",$PDS,10);
+    pdsDelete($PDS);
+
+    if($RC == NIME_OK) {
+        my $ROBOTS_PDS = Nimbus::PDS->new($NMS_RES);
+        my %RobotsList = ();
+        for( my $count = 0; my $ROBOTNFO = $ROBOTS_PDS->getTable("robotlist",PDS_PDS,$count); $count++) {
+            my $ROBOT = new perluim::robot($ROBOTNFO);
+            $RobotsList{$ROBOT->{name}} = $ROBOT;
+        }
+        return $RC,%RobotsList;
+    }
+    else {
+        return $RC,undef;
+    }
+}
+
+sub local_robotsHash {
+    my ($self) = @_;
+    my $PDS = pdsCreate();
+    my ($RC,$NMS_RES) = nimRequest("$self->{robotname}",48002,"getrobots",$PDS,10);
     pdsDelete($PDS);
 
     if($RC == NIME_OK) {
@@ -150,7 +176,24 @@ sub getEnv {
     if(defined($var)) {
         pdsPut_PCH ($PDS,"variable","$var");
     }
-    my ($RC,$RES) = nimNamedRequest("$self->{clean_addr}/controller","get_environment",$PDS,10);
+    my ($RC,$RES) = nimNamedRequest("$self->{clean_addr}/controller","get_environment",$PDS);
+    pdsDelete($PDS);
+
+    if($RC == NIME_OK) {
+        my $Hash = Nimbus::PDS->new($RES)->asHash();
+        return $RC,$Hash;
+    }
+    return $RC,undef;
+}
+
+sub local_getEnv {
+    my ($self,$var) = @_;
+
+    my $PDS = pdsCreate();
+    if(defined($var)) {
+        pdsPut_PCH ($PDS,"variable","$var");
+    }
+    my ($RC,$RES) = nimRequest("$self->{robotname}",48000,"get_environment",$PDS);
     pdsDelete($PDS);
 
     if($RC == NIME_OK) {
@@ -164,7 +207,21 @@ sub getInfo {
     my ($self) = @_;
 
     my $PDS = pdsCreate();
-    my ($RC,$RES) = nimNamedRequest("$self->{clean_addr}/controller","get_info",$PDS,10);
+    my ($RC,$RES) = nimNamedRequest("$self->{clean_addr}/controller","get_info",$PDS);
+    pdsDelete($PDS);
+
+    if($RC == NIME_OK) {
+        my $Hash = Nimbus::PDS->new($RES)->asHash();
+        return $RC,$Hash;
+    }
+    return $RC,undef;
+}
+
+sub local_getInfo {
+    my ($self) = @_;
+
+    my $PDS = pdsCreate();
+    my ($RC,$RES) = nimRequest("$self->{robotname}",48000,"get_info",$PDS);
     pdsDelete($PDS);
 
     if($RC == NIME_OK) {
@@ -198,13 +255,44 @@ sub tunnelsList {
     return $RC,undef;
 }
 
-sub queueState {
+sub local_tunnelsList {
+    my ($self) = @_; 
+
+    my $PDS = pdsCreate();
+    my ($RC,$RES) = nimRequest("$self->{robotname}",48002,"tunnel_get_info",$PDS,5);
+    pdsDelete($PDS);
+    if($RC == NIME_OK) {
+        my @Tunnels = ();
+        my $TunnelPDS = Nimbus::PDS->new($RES);
+        for( my $count = 0; my $T = $TunnelPDS->getTable("tunnels",PDS_PDS,$count); $count++) {
+            my $TunnelObject = new perluim::tunnels($T);
+            push(@Tunnels,$TunnelObject);
+        }
+        return $RC,@Tunnels;
+    }
+    
+    return $RC,undef;
+}
+
+sub setQueue_state {
     my ($self,$queueName,$state) = @_; # State equal 1/0
 
     my $PDS = pdsCreate();
     pdsPut_PCH($PDS,"queue","$queueName");
     pdsPut_INT($PDS,"active",$state);
-    my ($RC,$RES) = nimNamedRequest("$self->{addr}","queue_active",$PDS,5);
+    my ($RC,$RES) = nimNamedRequest("$self->{addr}","queue_active",$PDS);
+    pdsDelete($PDS);
+
+    return $RC;
+}
+
+sub local_setQueue_state {
+    my ($self,$queueName,$state) = @_; # State equal 1/0
+
+    my $PDS = pdsCreate();
+    pdsPut_PCH($PDS,"queue","$queueName");
+    pdsPut_INT($PDS,"active",$state);
+    my ($RC,$RES) = nimRequest("$self->{robotname}",48002,"queue_active",$PDS);
     pdsDelete($PDS);
 
     return $RC;
@@ -212,7 +300,23 @@ sub queueState {
 
 sub queueDelete {
     my ($self,$queueName) = @_;
+    if(not defined $queueName) {
+        return 1;
+    }
 
+    my $PDS = pdsCreate();
+    pdsPut_PCH($PDS,"queue","$queueName");
+    my ($RC,$RES) = nimNamedRequest("$self->{addr}","queue_delete",$PDS,5);
+    pdsDelete($PDS);
+
+    return $RC;
+}
+
+sub local_queueDelete {
+    my ($self,$queueName) = @_;
+    if(not defined $queueName) {
+        return 1;
+    }
     my $PDS = pdsCreate();
     pdsPut_PCH($PDS,"queue","$queueName");
     my ($RC,$RES) = nimNamedRequest("$self->{addr}","queue_delete",$PDS,5);
@@ -225,7 +329,26 @@ sub queueList {
     my ($self) = @_;
 
     my $PDS = pdsCreate();
-    my ($RC,$RES) = nimNamedRequest("$self->{addr}","queue_list",$PDS,5);
+    my ($RC,$RES) = nimNamedRequest("$self->{addr}","queue_list",$PDS);
+    pdsDelete($PDS);
+    if($RC == NIME_OK) {
+        my @Queue = ();
+        my $Queue_PDS = Nimbus::PDS->new($RES);
+        for( my $count = 0; my $Q = $Queue_PDS->getTable("sections",PDS_PDS,$count); $count++) {
+            my $QueueObject = new perluim::queue($Q);
+            push(@Queue,$QueueObject);
+        }
+        return $RC,@Queue;
+    }
+    return $RC,undef;
+
+}
+
+sub local_queueList {
+    my ($self) = @_;
+
+    my $PDS = pdsCreate();
+    my ($RC,$RES) = nimRequest("$self->{robotname}",48002,"queue_list",$PDS);
     pdsDelete($PDS);
     if($RC == NIME_OK) {
         my @Queue = ();
@@ -242,9 +365,24 @@ sub queueList {
 
 sub probeVerify {
     my ($self,$probeName) = @_;
+    if(not defined $probeName) {
+        return 1;
+    }
     my $PDS = pdsCreate();
     pdsPut_PCH($PDS,"name","automated_deployment_engine");
-    my ($RC,$NMS_RES) = nimNamedRequest("$self->{clean_addr}/controller","probe_verify",$PDS,10);
+    my ($RC,$NMS_RES) = nimNamedRequest("$self->{clean_addr}/controller","probe_verify",$PDS);
+    pdsDelete($PDS);
+    return $RC;
+}
+
+sub local_probeVerify {
+    my ($self,$probeName) = @_;
+    if(not defined $probeName) {
+        return 1;
+    }
+    my $PDS = pdsCreate();
+    pdsPut_PCH($PDS,"name","automated_deployment_engine");
+    my ($RC,$NMS_RES) = nimRequest("$self->{robotname}",48000,"probe_verify",$PDS);
     pdsDelete($PDS);
     return $RC;
 }
