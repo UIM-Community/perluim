4b450f4702ff6fa4f78803bd11f3c86af4d9137f
Gentilhomme <gentilhomme.thomas@gmail.com>
2016-12-27 11:13:01 +0100

    New methods to generate nimsoft Alarm with spooler

--- utils.pm
+++ utils.pm
@@ -10,7 +10,7 @@ use Nimbus::Session;
 use Nimbus::CFG;
 use Nimbus::PDS;
 
-our @EXPORT_OK = qw(minIndex getTerminalInput getDate randomString createDirectory doSleep);
+our @EXPORT_OK = qw(minIndex getTerminalInput getDate rndStr createDirectory doSleep generateAlarm nimId);
 
 sub doSleep {
     my ($self,$sleepTime) = @_;
@@ -31,13 +31,6 @@ sub createDirectory {
         }
         $track .= "$_/";
     }
-}
-
-sub randomString {
-    my ($length) = @_; 
-    my @set = ('0' ..'9', 'A' .. 'F');
-    my $str = join '' => map $set[rand @set], 1 .. $length;
-    return $str;
 }
 
 sub minIndex {
@@ -65,3 +58,50 @@ sub getDate {
 	$timestamp     =~ s/://g;
     return $timestamp;
 }
+
+sub rndStr { 
+    return join'', @_[ map{ rand @_ } 1 .. shift ] 
+}
+
+sub nimId {
+    my $A = rndStr(10,'A'..'Z',0..9);
+    my $B = rndStr(5,0..9);
+    return "$A-$B";
+}
+
+sub generateAlarm {
+    my ($subject,$hashRef) = @_;
+
+    my $PDS = pdsCreate(); 
+    my $nimid = nimId();
+
+    pdsPut_PCH($PDS,"nimid",$nimid);
+    pdsPut_INT($PDS,"nimts",time());
+    pdsPut_INT($PDS,"tz_offset",-3600);
+    pdsPut_PCH($PDS,"subject","$subject");
+    #pdsPut_PCH($PDS,"md5sum","");
+    pdsPut_PCH($PDS,"user_tag_1",$hashRef->{usertag1} || "");
+    pdsPut_PCH($PDS,"user_tag_2",$hashRef->{usertag2} || "");
+    pdsPut_PCH($PDS,"source",$hashRef->{source});
+    pdsPut_PCH($PDS,"robot",$hashRef->{robot} || "");
+    pdsPut_PCH($PDS,"prid",$hashRef->{probe} || "");
+    pdsPut_INT($PDS,"pri",$hashRef->{severity} || 1);
+    pdsPut_PCH($PDS,"dev_id",$hashRef->{dev_id} || "");
+    pdsPut_PCH($PDS,"met_id",$hashRef->{met_id} || "");
+    pdsPut_PCH($PDS,"supp_key",$hashRef->{supp_key} || "1");
+    pdsPut_PCH($PDS,"suppression",$hashRef->{suppression} || "");
+    pdsPut_PCH($PDS,"origin",$hashRef->{origin} || "");
+    pdsPut_PCH($PDS,"domain",$hashRef->{domain} || "");
+
+    my $AlarmPDS = pdsCreate();
+    pdsPut_INT($AlarmPDS,"level",$hashRef->{severity} || 1);
+    pdsPut_PCH($AlarmPDS,"message",$hashRef->{message});
+    pdsPut_PCH($AlarmPDS,"subsys",$hashRef->{subsystem} || 1);
+    if(defined $hashRef->{token}) {
+        pdsPut_PCH($AlarmPDS,"token",$hashRef->{token});
+    }
+
+    pdsPut_PDS($PDS,"udata",$AlarmPDS);
+
+    return ($PDS,$nimid);
+}   
